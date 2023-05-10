28227c324720780e1b104f8bf8720f34a1789916
bjs <127002178+benstorlie@users.noreply.github.com>
2023-05-09 21:42:22 -0700

    I should have been committing more often here. now trying data.json file

--- script.js
+++ script.js
@@ -1,39 +1,15 @@
-let sampledata = [
-  {    
-    content: "Item 1",
-    inset: "inset1",    
-    items: [
-      {        
-        content: "Subitem 1.1"      
-      },      
-      {        
-        content: "Subitem 1.2"      
-      }    
-    ]
-  },
-  {
-    content: "Item 2",
-    items: [
-      {
-        content: "Subitem 2.1"
-      },
-      {
-        content: "Subitem 2.2",
-        inset: "inset2",
-        items: [
-          {
-            content: "Subsubitem 2.2.1"
-          },
-          {
-            content: "Subsubitem 2.2.2"
-          }
-        ]
-      }
-    ]
-  }
-];
+import htmlData from '.data.json' assert { type: 'json'};
 
-
+/**
+ * user-created data
+ * @typedef {Object} OrganizerData
+ * @property {String} main - Emmet abbreviation, most identifiable information to include in the diagram, accepts html
+ * @property {String} [content] - Emmet abbreviation, the rest of the properties of the element, included in list but not diagram, accepts html
+ * @property {Number} hue - Number [0,360] to include in hsl
+ * @property {String} [text] - html to be put inside the box, such as the x inside of a close button
+ * @property {OrganizerData[]} [children] - the sub-element of the given element
+ * @property {{liClass?:String,liStyle?:String,ulClass?:String,ulStyle?:String}} [diagramFormat] - strings to add to the html for formatting the diagram. 'li' is for the unbordered box containing the Emmet abbrev. and the bordered box. 'ul' is the bordered box.
+*/
 
 window.onload = function() {
 
@@ -43,46 +19,62 @@ window.onload = function() {
 
 function buildNestedList($listContainer,$divContainer,data) {
 
-  ulRecursive($listContainer,$divContainer,data);
+  // ulRecursive($listContainer,$divContainer,data);
+  ulRecursiveListId($listContainer,$divContainer,data);
+  scrollToId()
 }
 
-function ulRecursive($superLi,$superDivLi,data) {
+function ulRecursiveListId($li,$divLi,data,listId='') {
 
-  let $ul = $('<ul>');
+  const {ulClass='',ulStyle='',children=[],text=''} = data;
+
+  let $ul = $('<ul>',{id: 'ul'+listId});
   let $divUl = $('<div>',{
+    id: 'dul'+listId,
     //class: 'ul',
-    class: `ul ${data.ulClass}`,
-    style: data.ulStyle,
-    html: data.text
+    class: `ul ${ulClass}`,
+    style: ulStyle,
+    html: text
   });
 
-  if (data.children) {
-    data.children.forEach(item => {
-      liRecursive($ul,$divUl,item);
+  if (children) {
+    children.forEach((item,index) => {
+      liRecursiveListId($ul,$divUl,item,(listId ? listId+index : ''));
     });
     
   } else {
     $divUl.addClass('end');
   }
 
-  $superLi.append($ul);
-  $superDivLi.append($divUl);
+  $li.append($ul);
+  $divLi.append($divUl);
 
 }
 
-function liRecursive($superUl,$superDivUl,data) {
+/**
+ * 
+ * @param {JQuery} $superUl <ul> element to attach the new <li> to
+ * @param {JQuery} $superDivUl <div class='ul'> element to attach the new <div class='li'> to
+ * @param {OrganizerData} data the user-created data
+ * @param {String} listId the suffix to attach to the id's of new elements
+ */
+function liRecursiveListId($ul,$divUl,data,listId='') {
 
-  const {main,content,text,inset,hue,liStyle,liClass}=data;
+  const {main,content='',inset=false,hue,liStyle='',liClass=''}=data;
+
+  let html =`<span ${(listId ? `id='liText${listId}'` : '')}" class='liText'><b>${main}</b>${content}</span>`;
 
   let $li = $('<li>',{
-    html: `<b>${main}</b>${(content ? content : '')}`,
+    id: 'li'+listId,
+    html: html,
   });
 
   let $divLi = $('<div>', {
+    id: 'dil'+listId,
     //class: 'li',
     class: `li ${liClass}`,
     style: liStyle,
-    html: main
+    html: `<span ${(listId ? `id='liText${listId}'` : '')}" class='dliText'>${main}</span>`
   });
 
   $li.css('color',color(hue));
@@ -92,18 +84,42 @@ function liRecursive($superUl,$superDivUl,data) {
     let $divLiInset = $divLi.clone();
     $divLiInset.append($('<div>',{class: 'ul see-inset', html: "see inset"}));
 
-    $superDivUl.append($divLiInset);
-    ulRecursive($li,$divLi,data);
+    $divUl.append($divLiInset);
+    ulRecursiveListId($li,$divLi,data,listId);
 
     let insetDiagram = $(`#${inset}-diagram`);
+    if (!insetDiagram) {
+      insetDiagram = $('<section>',{class:'boxes inset',id: inset+'-diagram'});
+      $('body').append(insetDiagram);
+    }
     insetDiagram.append($divLi);
-    $superUl.append($li);
+    $ul.append($li);
 
   } else {
-    ulRecursive($li,$divLi,data);
-    $superDivUl.append($divLi);
-    $superUl.append($li);
+    ulRecursiveListId($li,$divLi,data,listId);
+    $divUl.append($divLi);
+    $ul.append($li);
   }
+}
+
+/** 
+* Adds click events to $(".liText") and $(".dliText") Clicking either element scrolls to the other element with the matching id.
+*/
+function scrollToId() {
+  $(".liText").click(function() {
+    var divId = $(this).attr("id").replace("li", "dli");
+    $('html, body').animate({
+      scrollTop: $("#" + divId).offset().top
+    }, 500);
+  });
+
+  $(".dliText").click(function() {
+    var liId = $(this).attr("id").replace("dli", "li");
+    $('html, body').animate({
+      scrollTop: $("#" + liId).offset().top
+    }, 500);
+  });
+  
 }
 
 
